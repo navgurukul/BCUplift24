c3b83c9237b082cd0b804815946bdbd77b82ab1d
Umang Doctor <emailumangdoctor@gmail.com>
2023-10-31 23:22:17 +0530

    Update Exam1.java

--- Jainab/Exam1.java
+++ Jainab/Exam1.java
@@ -1,1 +1,67 @@
+import java.util.ArrayList;
+
+public class Exam1{
+	public static void main(String [] args){
+		Account a1=new Account();
+		A1.createTransaction(transactions);
+		Customer c1=new Customer();
+		
+		
+	
+	}
+
+}
+class Customer{
+	private String name;
+	private int dob;
+	private int adhaarNumber;
+	private account Account;
+	
+	
+}
+class Account{
+	private int accountNumber;
+	private double balance;
+	ArrayList<String> transactions= new ArrayList<String>();
+
+	void deposit(){
+
+	}
+	void withdaw(){
+
+	}
+	void createTransaction(){
+
+	}
+	void printTransactions(){
+
+	}
+}
+class Transaction{
+	int id;
+	int date;
+	String type;
+	int amount;
+
+}
+class CurrentAccount extends Account{
+	int overdraftLimit;
+
+	void withdraw(){
+
+	}
+	void createTransaction(){
+
+	}
+
+}
+class SavingAccount{
+	int interestRate;
+	void endOfDayinterest(){
+
+	}
+
+
+}
+
 
