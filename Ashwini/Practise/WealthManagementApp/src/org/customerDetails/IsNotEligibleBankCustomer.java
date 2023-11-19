41a9fbb8cb8fccaf7abf5c2a5f19f6c2e6df3f57
ashwinighongade <95064067+ashwinighongade@users.noreply.github.com>
2023-11-19 09:28:45 +0530

    Delete Ashwini/Practise/WealthManagementApp directory

--- Ashwini/Practise/WealthManagementApp/src/org/customerDetails/IsNotEligibleBankCustomer.java
+++
@@ -1,8 +0,0 @@
-package org.customerDetails;
-
-public class IsNotEligibleBankCustomer extends Exception{
-    public IsNotEligibleBankCustomer(String message) {
-        super(message);
-    }
-
-}
