59bfe4851bdeacb9cbe598e62e133e6a171ecb66
Kajal Gupta <kajalg22@navgurukul.org>
2023-12-20 20:00:42 +0530

    Added everything

--- Kajal/Practice/OnlineBanking/src/main/java/org/uplift/Main.java
+++ Kajal/Practice/OnlineBanking/src/main/java/org/uplift/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("New things");
-        System.out.println("Hello in Online Banking Service!");
+        System.out.println("Welcome in Online Banking Service!");
     }
 }
