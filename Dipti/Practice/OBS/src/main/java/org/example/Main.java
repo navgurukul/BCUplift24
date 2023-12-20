633e4e8d4f769a13bb29af702de2505ffc80921e
dipti <thakre123dipti@gmail.com>
2023-12-20 16:47:25 +0530

    updated

--- Dipti/Practice/OBS/src/main/java/org/example/Main.java
+++ Dipti/Practice/OBS/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("commited to intellij");
+        System.out.println("commited  intellije");
 
 
     }
