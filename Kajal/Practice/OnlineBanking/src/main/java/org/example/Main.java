07fb2d7b2f236d980888393e7fee62f6731c0897
Kajal Gupta <kajalg22@navgurukul.org>
2023-12-20 17:22:36 +0530

    New thing adde

--- Kajal/Practice/OnlineBanking/src/main/java/org/example/Main.java
+++ Kajal/Practice/OnlineBanking/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("New things");
         System.out.println("Hello in Online Banking Service!");
     }
 }
