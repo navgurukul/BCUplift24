8ddc763d244d18f3c87008c18562938a5424d4f9
Neha saraf <nehasaraf060601@gmail.com>
2023-12-20 23:36:23 +0530

    Updated Code

--- NehaSaraf/Practice/OBS/src/main/java/org/example/bank/TransactionManager.java
+++ NehaSaraf/Practice/OBS/src/main/java/org/example/bank/TransactionManager.java
@@ -4,21 +4,36 @@ import org.example.account.Account;
 import org.example.account.InsufficientBalanceException;
 import org.example.account.Transaction;
 
-import java.util.Date;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
 
 public class TransactionManager {
     private Random random;
+    private Set<Transaction> transactionHistory;
+
+    public TransactionManager() {
+        this.transactionHistory = new HashSet<>();
+    }
+
     public Transaction transfer(Account source, Account target, double amount) throws InsufficientBalanceException {
         source.withdraw(amount);
         target.deposit(amount);
         String txtId=""+random.nextInt(1000000,100000000);
-        return new Transaction(source,target,new Date(),amount,txtId);
+        Transaction t=new Transaction(source,target,new Date(),amount,txtId);
+        transactionHistory.add(t);
+        return t;
     }
 
     public void setRandomNo(Random random) {
         this.random = random;
+
     }
 
+    public Transaction findByTransactionId(String s) {
+        for(Transaction t:transactionHistory){
+            if(t.getId().equals(s))
+                return t;
+
+        }
+        return null;
+    }
 }
