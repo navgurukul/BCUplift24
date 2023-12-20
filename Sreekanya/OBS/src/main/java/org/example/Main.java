dab0705265d857fd3c0668d8deabc804f3cc8a4a
sreekanya chagaleti <sreekanyachagaleti21@navgurukul.org>
2023-12-20 17:29:03 +0530

    first commit

--- Sreekanya/OBS/src/main/java/org/example/Main.java
+++ Sreekanya/OBS/src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         //System.out.println("Hello world! ");
-        System.out.println("first change");
+        System.out.println("first change in my branch");
     }
 }
