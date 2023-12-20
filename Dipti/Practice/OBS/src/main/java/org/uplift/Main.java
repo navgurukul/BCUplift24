53c594bdf58889d6d8f30b17a1de5342c41c6259
dipti <thakre123dipti@gmail.com>
2023-12-20 17:36:12 +0530

    new branch2

--- Dipti/Practice/OBS/src/main/java/org/uplift/Main.java
+++ Dipti/Practice/OBS/src/main/java/org/uplift/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.uplift;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("commited  intellije");
         System.out.println("commited to dipti-obs");
-
+        System.out.println("added a new branch");
     }
 }
