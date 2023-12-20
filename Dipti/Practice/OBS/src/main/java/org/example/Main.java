d436e7f8a45172b83788e5a25912975d78650b1b
dipti <thakre123dipti@gmail.com>
2023-12-20 16:57:49 +0530

    added a change1

--- Dipti/Practice/OBS/src/main/java/org/example/Main.java
+++ Dipti/Practice/OBS/src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("commited  intellije");
-
+        System.out.println("commited to dipti-obs");
 
     }
 }
