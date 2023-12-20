4610ad70d8777e26a6460beb1f5c750399cbce57
unknown <dagwarnikita@gmail.com>
2023-12-20 16:44:40 +0530

    Upate main

--- Nikita/Practice/OBS/src/main/java/org/example/Main.java
+++ Nikita/Practice/OBS/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello nikita");
+        System.out.println("Hello Nikita Dagwar");
     }
 }
