cd639c7cea95e96bd78e84bb7aec7e187c750e7d
Priyanka <priyankadhomne22@navgurukul.org>
2023-12-20 19:48:36 +0530

    Implemented all

--- Priyanka/OBS/src/main/java/org/uplift/account/SavingAccount.java
+++ Priyanka/OBS/src/main/java/org/uplift/account/SavingAccount.java
@@ -1,2 +1,43 @@
-package org.uplift.account;public class SavingAccount {
+package org.uplift.account;
+
+import org.uplift.account.exception.InsufficientBalanceException;
+import org.uplift.account.user.User;
+
+import java.util.Date;
+
+public class SavingAccount implements Account {
+
+    private User user;
+    private String accountNumber;
+    private double balance;
+    private Date openingDate;
+    private double overdraftLimit;
+
+    public SavingAccount(User user, String accountNumber, double balance, Date openingDate, double overdraftLimit) {
+        this.user = user;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.openingDate = openingDate;
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public double showBalance() {
+        return balance;
+    }
+
+    @Override
+    public double deposit(double amount) {
+        balance += amount;
+        return balance;
+    }
+
+    @Override
+    public double withdraw(double amount) throws InsufficientBalanceException {
+        if (amount>balance+overdraftLimit) {
+            throw new InsufficientBalanceException("Insufficient balance Exception");
+        }
+        balance -= amount;
+        return balance;
+    }
 }
--- Priyanka/OBS/src/test/java/org/uplift/account/SavingAccountTest.java
+++ Priyanka/OBS/src/test/java/org/uplift/account/SavingAccountTest.java
@@ -1,4 +1,49 @@
+package org.uplift.account;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.uplift.account.exception.InsufficientBalanceException;
+import org.uplift.account.user.User;
+
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SavingAccountTest {
-  
+    private SavingAccount sa;
+    private User user;
+    private static final double Account_Balance = 50000.0;
+    private static final double OD_Limit = 500;
+    @BeforeEach
+    void setup() {
+        user = new User("priyanka","9075367109","priyankaadhomne@423","PriyankaDhomne","Priya@123");
+        sa = new SavingAccount(user,"1233Ac",Account_Balance,new Date(),500.0 );
+    }
+
+    @Test
+    void showBalance() {
+        assertEquals(Account_Balance,sa.showBalance());
+    }
+
+    @Test
+    void deposit() {
+        double expectedBalance = Account_Balance + 1000;
+        assertEquals(expectedBalance, sa.deposit(1000));
+    }
+
+    @Test
+    void withdraw() throws InsufficientBalanceException {
+        double expectedBalance = Account_Balance - 1000;
+        assertEquals(expectedBalance,sa.withdraw(1000));
+    }
+
+    @Test
+    void withdrawWithOverdraftLimit() throws InsufficientBalanceException {
+        double expectedBalance = - 500;
+        assertEquals(expectedBalance,sa.withdraw(Account_Balance+OD_Limit));
+    }
+    @Test
+    void withdrawWithOOLimitBreach() {
+        assertThrows(InsufficientBalanceException.class,()->sa.withdraw(Account_Balance+OD_Limit+1));
+    }
 }
