98ba3497a7712c995fb46540b702a6a858d1656d
Gayatri <gayatriganorkar22@navgurukul.org>
2023-12-20 17:20:34 +0530

    upload

--- Gayatri/Practies/obs/src/main/java/org/example/Main.java
+++ Gayatri/Practies/obs/src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Commited in gayatri-obs branch");
+        System.out.println("Hello");
     }
 }
