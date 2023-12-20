2ed46ed2aed40d1d659c49037e40a2a05fa3c250
Musharrat <musharrat22@navgurukul.org>
2023-12-20 17:20:48 +0530

    changed code

--- Musharrat/Practice/OBS/src/main/java/org/example/Main.java
+++ Musharrat/Practice/OBS/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Changed code");
+        System.out.println("This is the third time that I changed code");
     }
 }
