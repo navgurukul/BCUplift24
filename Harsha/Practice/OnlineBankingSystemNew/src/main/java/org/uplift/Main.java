cc9504374f2728bfdc8067369ea4c3ad8dca3640
Harsha Chandurkar <harshachandurkar22@navgurukul.org>
2023-12-20 17:40:09 +0530

    branch created

--- Harsha/Practice/OnlineBankingSystemNew/src/main/java/org/uplift/Main.java
+++ Harsha/Practice/OnlineBankingSystemNew/src/main/java/org/uplift/Main.java
@@ -1,10 +1,10 @@
-package org.example;
+package org.uplift;
 
 //Online Banking System
 //Design and implement an online banking system
 //with features like account management, fund transfer,
 //transaction history, and security measures like
-//OTP verification or two-factor authentication.
+//OTP verification or two-factor authentication.
 public class Main {
     public static void main(String[] args) {
         System.out.println("1");
