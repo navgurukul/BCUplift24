582f403499f9f9e7c10f76a864f403fa805ff491
Ashwini Ghongade <goldy22@navgurukul.org>
2023-12-21 16:55:17 +0530

    Testing

--- Ashwini/Practice/OBC/src/main/java/org/uplift/uplifte/exceptionhandling/InsufficientBalanceException.java
+++ Ashwini/Practice/OBC/src/main/java/org/uplift/uplifte/exceptionhandling/InsufficientBalanceException.java
@@ -1,6 +1,7 @@
 package org.uplift.uplifte.exceptionhandling;
 
 public class InsufficientBalanceException extends Exception {
+    //Insufficient
     public InsufficientBalanceException(String message){
         super(message);
     }
