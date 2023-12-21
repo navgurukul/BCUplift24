2d0f721d3a72004897b8831e6eae0957d34595c3
Janhvi Shende <janhvishende24@gmail.com>
2023-12-22 00:14:44 +0530

    Updated OTP Management with implementation of methods

--- Janhvi/practice/OBS/src/test/java/org/uplift/banking/RetailBankingProviderTest.java
+++ Janhvi/practice/OBS/src/test/java/org/uplift/banking/RetailBankingProviderTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.uplift.exception.OTPExpiredException;
+
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class RetailBankingProviderTest {
 
@@ -17,6 +20,10 @@ class RetailBankingProviderTest {
     private static final String USERNAME = "abcd";
     @Mock
     private TransactionManager tm;
+
+    @Mock
+    private Scanner scanner;
+
 
     @InjectMocks
     private RetailBankingProvider rbp;
@@ -30,6 +37,16 @@ class RetailBankingProviderTest {
     void makePayment() throws Exception {
         rbp.makePayment(SOURCE_ACC_ID,TransferType.ACCOUNT_ID, USERNAME,TransferType.USERNAME,34365);
         verify(tm,times(1)).makePayment("A1234",TransferType.ACCOUNT_ID,USERNAME,TransferType.USERNAME,34365);
+
+    }
+
+
+    @Test
+    void makePaymentHandledOtpException() throws Exception {
+        when(scanner.next()).thenReturn(Attempt.YES.toString());
+        when(tm.makePayment(SOURCE_ACC_ID,TransferType.ACCOUNT_ID, USERNAME,TransferType.USERNAME,34365)).thenThrow(OTPExpiredException.class);
+        rbp.makePayment(SOURCE_ACC_ID,TransferType.ACCOUNT_ID, USERNAME,TransferType.USERNAME,34365);
+        verify(tm,atLeast(2)).makePayment(SOURCE_ACC_ID,TransferType.ACCOUNT_ID, USERNAME,TransferType.USERNAME,34365);
 
     }
 }
